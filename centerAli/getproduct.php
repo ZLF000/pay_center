5f8be4a92af7c5ebc3133f730e41f361c03ac763
zhanglanfan <2660284735@qq.com>
2020-07-03 18:13:35 +0800

    fix

--- centerAli/getproduct.php
+++ centerAli/getproduct.php
@@ -10,11 +10,12 @@ if ($_GET['mid']) {
 $result1 = mysqli_query($conn, $sql);
 
 while ($price = mysqli_fetch_assoc($result1)) {
-    if ($_GET['mid']) {
+    if($_GET['mid']) {
         $sql = "SELECT * FROM `sl_product` WHERE P_mid IN (" . $_GET['mid'] . ") AND P_del = 0 AND P_price = " . $price['P_price'];
     } else {
         $sql = "SELECT * FROM `sl_product` WHERE P_del = 0 AND P_price = " . $price['P_price'];
     }
+
     $products = mysqli_query($conn, $sql);
     $proArr = [];
     while($row = mysqli_fetch_assoc($products)) {
